c3e7a57ecd0d1a1dfffffd650df038061d760275
RibsLi <2234692411@qq.com>
2021-11-01 17:05:38 +0800

    初始化

---
+++ src/common/remSize.js
@@ -0,0 +1,16 @@
+function remSize() {
+  let deviceWidth = document.documentElement.clientWidth || window.innerWidth
+  if(deviceWidth <= 320) {
+    deviceWidth = 320
+  }
+  if(deviceWidth >= 750) {
+    deviceWidth = 750
+  }
+  document.documentElement.style.fontSize = (deviceWidth / 7.5) + 'px'
+  document.querySelector(body).style.fontSize = 0.16 + 'rem'
+}
+remSize()
+
+window.onreset = function() {
+  remSize()
+}
